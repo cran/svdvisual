b4700836328b38298d45f64cb00a7e388dac9c34
Lingsong Zhang <lingsong@purdue.edu>
2013-12-15 00:00:00 +0000

    version 1.1

--- R/svd3dplot.R
+++ R/svd3dplot.R
@@ -1,5 +1,5 @@
 svd3dplot <-
-function(data, ncomp=3, isurface=T, iimage=F, xlab='Column', ylab='Row', zlab='', ...){
+function(data, ncomp=3, irow=F, icol=F, isurface=T, iimage=F, xlab='Column', ylab='Row', zlab='', ...){
 # this is the svd three dimensional plot
 
 
@@ -14,12 +14,44 @@ nrow=data.dim[1];
 ncol=data.dim[2];
 ncell=nrow*ncol;
 
-data.svd<-svd(data, nu=ncomp, nv=ncomp);
+#make sure which mean will be removed.
+meanmat<-NULL
+localdata=data
+idouble=(irow & icol);
+meantitle=''
+if (idouble){
+  #calculate the double mean
+  meanmat<-doublemean(data)
+  localdata=data-meanmat;
+  meantitle='Mean (Double)'
+}
+
+if (irow & (!idouble)){
+  #calculate the row mean (mean of the rows)
+  meanmat<-rowmean(data)
+  localdata=data-meanmat;
+  meantitle='Mean (Row)'
+}
+if (icol & (!idouble)){
+  #calculate the column mean (mean of the columns)
+  meanmat<-columnmean(data)
+  localdata=data-meanmat;
+  meantitle='Mean (Column)'
+}
+
+#if mean is chosen, reduce the number of component to ncomp-1
+if (irow | icol) localncomp=ncomp-1;
+if (!irow & !icol) localncomp=ncomp;
+
+data.svd<-svd(localdata, nu=localncomp, nv=localncomp);
 umat=data.svd$u;
 vmat=data.svd$v;
-svec=data.svd$d[1:ncomp];
+svec=data.svd$d[1:localncomp];
 
-app=umat%*%diag(svec)%*%t(vmat);
+localmeanmat=meanmat
+if (is.null(meanmat)) localmeanmat=rep(0, nrow) %*% t(rep(0, ncol))
+
+app=localmeanmat+umat%*%diag(svec)%*%t(vmat);
 res=data-app;
 rowmat=seq(1:nrow)%*% t(rep(1, ncol));
 colmat=rep(1, nrow) %*% t(seq(1, ncol));
@@ -27,8 +59,18 @@ colmat=rep(1, nrow) %*% t(seq(1, ncol));
 #generate ploting matrix
 plotmat<-data.frame(matvalue=as.vector(data), rows=as.vector(rowmat), columns=as.vector(colmat), label=rep("Original data", ncell));
 for (i in 1:ncomp){
-	tempmat=svec[i]*(umat[, i]%*% t(vmat[, i]));
-	plotmat<-rbind(plotmat, data.frame(matvalue=as.vector(tempmat), rows=as.vector(rowmat), columns=as.vector(colmat), label=rep(paste("SVD", i), ncell)));
+  if (!irow & !icol){
+  tempmat=svec[i]*(umat[, i]%*% t(vmat[, i]));
+	plotmat<-rbind(plotmat, data.frame(matvalue=as.vector(tempmat), rows=as.vector(rowmat),  columns=as.vector(colmat), label=rep(paste("SVD", i), ncell)));}
+  if (irow | icol){
+    if (i==1){
+      plotmat<-rbind(plotmat, data.frame(matvalue=as.vector(meanmat), rows=as.vector(rowmat), columns=as.vector(colmat), label=rep(meantitle)))
+    }
+    if (i>1){
+      tempmat=svec[i-1]*(umat[, i-1]%*% t(vmat[, i-1]));
+      plotmat<-rbind(plotmat, data.frame(matvalue=as.vector(tempmat), rows=as.vector(rowmat),  columns=as.vector(colmat), label=rep(paste("SVD", i-1), ncell)));      
+    }
+  }
 }
 	plotmat<-rbind(plotmat, data.frame(matvalue=as.vector(app), rows=as.vector(rowmat), columns=as.vector(colmat), label=rep("Approximation", ncell)));
 	plotmat<-rbind(plotmat, data.frame(matvalue=as.vector(res), rows=as.vector(rowmat), columns=as.vector(colmat), label=rep("Residual", ncell)));
